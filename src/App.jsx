592b6b58774f0138a49c3a8470b30475c5c39446
Hassan Reda <50552009+hassanredaa@users.noreply.github.com>
2024-04-29 12:51:38 +0300

    First

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <section id='core-concepts'>
+          <h2>Projects</h2>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
