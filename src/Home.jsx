797bc8c0bbfd821903912fd136987daf7ef006c3
Hassan Reda <50552009+hassanredaa@users.noreply.github.com>
2024-05-02 03:41:17 +0300

    new assets

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,20 @@
+import imge from "../assets/gg.png"
 import video from "../assets/Untitled Video (3).mp4"
 
-export default function Home(){
-    
-    return(
+export default function Home() {
+
+    return (
         <div className="background-video">
             <header>
-        <button className="test">Test</button>
+                <button className="button-primary">Home</button>
 
             </header>
-        <video autoPlay muted loop id="video-bg">
-            <source src={video} type="video/mp4" />
-        </video>
-        <div className="content">
-            {/* Your content here */}
+            <video autoPlay muted loop id="video-bg">
+                <source src={video} type="video/mp4" />
+            </video>
+            <div className="content">
+                {/* Your content here */}
+            </div>
         </div>
-    </div>
     )
 }
