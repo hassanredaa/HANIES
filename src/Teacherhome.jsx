d1c042003a709591da81e66aeaafcd3d03af99ec
Youssef khalil <youssefkhalil389.yw@gmail.com>
2024-05-12 22:31:38 +0300

    edit

--- src/Teacherhome.jsx
+++ src/Teacherhome.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Bar, Line } from 'react-chartjs-2';
 import ggImage from '../assets/ggw.png';
 import goodImage from '../assets/good.png';
 import avImage from '../assets/img_avatar.png';
+import * as data from './data.js';
+import statsd from '../assets/statsd.png';
+import top from '../assets/top.png';
 
 export default function Teacherhome() {
   
@@ -25,16 +30,38 @@ export default function Teacherhome() {
           <p style={{ textAlign: 'center' }}>Teacher</p>
 
           
-          <Link to="/Posts">View Posts</Link>
+          <Link to="/Posts" className="sidebar-button" style={{ marginTop: '15px' }}>View Posts</Link>
     
           
-          <Link to="/UpdateAccDonor" style={{ marginTop: '170px' }}>Update account</Link>
-            <Link to="/" style={{ marginTop: '170px' }}>Delete account</Link> 
-          <Link to="/" style={{ marginTop: '170px' }}>Log Out</Link>
+          <Link to="/UpdateAccDonor" className="sidebar-button" style={{ marginTop: '15px' }}>Update account</Link>
+            <Link to="/" className="sidebar-button" style={{ marginTop: '15px' }}>Delete account</Link> 
+          <Link to="/" className="sidebar-button" style={{ marginTop: '15px' }}>Log Out</Link>
         </div>
+        <div className="contentt" style={{}}>
+                <div className="text-content">
+                <h2 style={{ textAlign: 'left' }}>Teacher Dashboard</h2>
 
-        <h1 style={{ textAlign: 'left' }}>Welcome to Good Giving!</h1>
-        <h2 style={{ textAlign: 'left' }}>This is a place where you can make a difference.</h2>
+                    <div style={{display: 'flex'}}>
+                    <img src={statsd} style={{ height: '125px', marginRight: '-450px' }} />
+                    <img src={top} style={{ height: '200px', paddingLeft: '800px',}} />
+
+                    </div>
+
+                    <div className="chart-container" style={{ width: '100%' }}>
+                        <Line style={{width: '800px', marginLeft: '150px' }}
+                            data={{
+                                labels: ["Hassan","Amira", "Nada","Welo","Lolo","Khello"],
+                                datasets: [{
+                                    label: "Donors' Performance",
+                                    data: [10, 10,40,50,20,40],
+                                }]
+                            }}
+                        />
+                    </div>
+
+                    {/* <p>Select an option from the sidebar to get started.</p> */}
+                </div>
+            </div>
 
         
 
