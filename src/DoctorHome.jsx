9397f10bc56a12c3c0a679cad4e3eb3a41382d6c
Youssef khalil <youssefkhalil389.yw@gmail.com>
2024-05-12 22:47:09 +0300

    edit

--- src/DoctorHome.jsx
+++ src/DoctorHome.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Bar, Line } from 'react-chartjs-2';
 import ggImage from '../assets/ggw.png';
 import goodImage from '../assets/good.png';
 import avImage from '../assets/img_avatar.png';
+import * as data from './data.js';
+import statsd from '../assets/statsd.png';
+import top from '../assets/top.png';
 
 export default function DoctorHome() {
   // State to manage the toggles
@@ -29,27 +34,45 @@ export default function DoctorHome() {
             <img src={avImage} style={{ marginLeft: '5px', marginTop: '50px', borderRadius: '50%', width: '100px', height: '100px' }} />
           </div>
           <h1 style={{ textAlign: 'center', padding: '5px' }}>name</h1>
-          <p style={{ textAlign: 'center' }}>User</p>
+          <p style={{ textAlign: 'center' }}>Doctor</p>
 
-          <ul style={{ paddingTop: '20px' }}>
-            <li>
-
-            </li>
-            <li>
+          
+            
               
               
-                  <li><Link to="/Cases">View Cases</Link></li>
-                  <li><Link to="/FulfillMed">Fulfiill Cases</Link></li>
-                
-              
-            </li>
-
-          </ul>
-          <Link to="/" style={{ marginTop: '170px' }}>Log Out</Link>
+          <Link to="/Cases" className="sidebar-button" style={{ marginTop: '15px' }}>View Cases</Link>
+          <Link to="/FulfillMed" className="sidebar-button" style={{ marginTop: '15px' }}>Fulfill Cases</Link>
+          <Link to="/UpdateAccDonor" className="sidebar-button" style={{ marginTop: '15px' }}>Update account</Link>
+          <Link to="/" className="sidebar-button" style={{ marginTop: '15px' }}>Delete account</Link> 
+          <Link to="/" className="sidebar-button" style={{ marginTop: '15px' }}>Log Out</Link>
         </div>
 
-        <h1 style={{ textAlign: 'left' }}>Welcome to Good Giving!</h1>
-        <h2 style={{ textAlign: 'left' }}>This is a place where you can make a difference.</h2>
+        <div className="contentt" style={{}}>
+                <div className="text-content">
+                <h2 style={{ textAlign: 'left' }}>Doctor Dashboard</h2>
+
+                    <div style={{display: 'flex'}}>
+                    <img src={statsd} style={{ height: '125px', marginRight: '-450px' }} />
+                    <img src={top} style={{ height: '200px', paddingLeft: '800px',}} />
+
+                    </div>
+
+                    <div className="chart-container" style={{ width: '100%' }}>
+                        <Line style={{width: '800px', marginLeft: '150px' }}
+                            data={{
+                                labels: ["Hassan","Amira", "Nada","Welo","Lolo","Khello"],
+                                datasets: [{
+                                    label: "Donors' Performance",
+                                    data: [10, 10,40,50,20,40],
+                                }]
+                            }}
+                        />
+                    </div>
+
+                    {/* <p>Select an option from the sidebar to get started.</p> */}
+                </div>
+            </div>
+
 
               
       </div>
